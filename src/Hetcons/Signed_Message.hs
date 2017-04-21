2de822d99791f16c181d17e3040c0feda6c0a766
Isaac Sheff <isheff@cs.cornell.edu>
2017-04-21 18:07:50 -0400

    signing function exception cases now written, untested

--- src/Hetcons/Signed_Message.hs
+++ src/Hetcons/Signed_Message.hs
@@ -11,6 +11,7 @@ import Hetcons.Hetcons_Exception (Hetcons_Exception(Hetcons_Exception_No_Support
                                                    ,Hetcons_Exception_Descriptor_Does_Not_Match_Crypto_ID
                                                    ,Hetcons_Exception_No_Supported_Hash_Type_Descriptor_Provided
                                                    ,Hetcons_Exception_Descriptor_Does_Not_Match_Signed_Hash
+                                                   ,Hetcons_Exception_No_Supported_Crypto_ID_Type_Descriptor_Provided
                                                    ,Hetcons_Exception_Invalid_Signed_Hash))
 
 import Hetcons_Consts(sUPPORTED_HASH_SHA2_DESCRIPTOR
@@ -87,6 +88,11 @@ import Hetcons_Types (Signed_Message (Signed_Message)
                      ,Public_Crypto_Key_Type_Descriptor(Public_Crypto_Key_Type_Descriptor)
                         ,public_Crypto_Key_Type_Descriptor_public_crypto_key_x509
                         ,default_Public_Crypto_Key_Type_Descriptor
+                     ,No_Supported_Crypto_ID_Type_Descriptor_Provided
+                        ,no_Supported_Crypto_ID_Type_Descriptor_Provided_offending_crypto_id_type_descriptor
+                        ,no_Supported_Crypto_ID_Type_Descriptor_Provided_supported_crypto_id_type_descriptor
+                        ,no_Supported_Crypto_ID_Type_Descriptor_Provided_explanation
+                        ,default_No_Supported_Crypto_ID_Type_Descriptor_Provided
                      )
 
 import           Control.Monad.Error    ()
@@ -160,7 +166,7 @@ verify Signed_Message
 -- | If it's a public crypto key, but not an x509, we return an appropriate Exception
 verify Signed_Message
        {signed_Message_signature =
-          signed_hash@Signed_Hash
+          Signed_Hash
           {signed_Hash_crypto_id = Just
              Crypto_ID
              {crypto_ID_public_crypto_key = Just
@@ -178,7 +184,7 @@ verify Signed_Message
 -- | If it's a Crypto ID, but not a public crypto key, we return an appropriate Exception
 verify Signed_Message
        {signed_Message_signature =
-          signed_hash@Signed_Hash
+          Signed_Hash
           {signed_Hash_crypto_id = Just
              crypto_id@(Crypto_ID
              {crypto_ID_public_crypto_key = Nothing})}}
@@ -226,14 +232,11 @@ verify Signed_Message
        {signed_Message_signature =
           signed_hash@Signed_Hash
           {signed_Hash_hash_type_descriptor = Nothing}}
-  = Left $ Hetcons_Exception_Descriptor_Does_Not_Match_Signed_Hash
-             default_Descriptor_Does_Not_Match_Signed_Hash {
-                descriptor_Does_Not_Match_Signed_Hash_signed_hash_type_descriptor =
-                  sUPPORTED_SIGNED_HASH_TYPE_DESCRIPTOR
-               ,descriptor_Does_Not_Match_Signed_Hash_signed_hash =
-                  signed_hash
-               ,descriptor_Does_Not_Match_Signed_Hash_explanation =
-                  Just "Alas, I require a type descriptor in my signed hashes."}
+  = Left $ Hetcons_Exception_No_Supported_Hash_Type_Descriptor_Provided
+             default_No_Supported_Hash_Type_Descriptor_Provided  {
+               no_Supported_Hash_Type_Descriptor_Provided_offending_hash_type_descriptor = Nothing
+              ,no_Supported_Hash_Type_Descriptor_Provided_supported_hash_type_descriptor = Just sUPPORTED_HASH_TYPE_DESCRIPTOR
+              ,no_Supported_Hash_Type_Descriptor_Provided_explanation = Just "No Hash Type Descriptor provided"}
 
 
 
@@ -277,3 +280,161 @@ sign (Crypto_ID
                     {public_Crypto_Key_public_crypto_key_x509 = Just public_key}}
               ,signed_Hash_hash_type_descriptor = Just default_Hash_Type_Descriptor
                    {hash_Type_Descriptor_sha2 = Just $ singleton max_length}}}}
+
+
+-- | If the Crypto_ID is a key, but not an X509
+sign (Crypto_ID
+       {crypto_ID_public_crypto_key = Just
+          public_key@Public_Crypto_Key
+          {public_Crypto_Key_public_crypto_key_x509 = Nothing}})
+      _ _ _ _
+  = Left $ Hetcons_Exception_Descriptor_Does_Not_Match_Public_Crypto_Key
+             default_Descriptor_Does_Not_Match_Public_Crypto_Key {
+                descriptor_Does_Not_Match_Public_Crypto_Key_public_crypto_key_type_descriptor =
+                  sUPPORTED_PUBLIC_CRYPTO_KEY_TYPE_DESCRIPTOR
+               ,descriptor_Does_Not_Match_Public_Crypto_Key_public_crypto_key =
+                  public_key
+               ,descriptor_Does_Not_Match_Public_Crypto_Key_explanation =
+                  Just "Alas, I only support X509 Public Crypto Keys at this time."}
+
+-- | If the crypto ID isn't a public key
+sign crypto_id@(Crypto_ID {crypto_ID_public_crypto_key = Nothing}) _ _ _ _ =
+  Left $ Hetcons_Exception_Descriptor_Does_Not_Match_Crypto_ID
+           default_Descriptor_Does_Not_Match_Crypto_ID {
+              descriptor_Does_Not_Match_Crypto_ID_crypto_id_type_descriptor =
+                sUPPORTED_CRYPTO_ID_TYPE_DESCRIPTOR
+             ,descriptor_Does_Not_Match_Crypto_ID_crypto_id =
+                crypto_id
+             ,descriptor_Does_Not_Match_Crypto_ID_explanation =
+                Just "Alas, I require crypto IDs to be full X509 Public Crypto Keys at this time."}
+
+-- | If the Signed_Hash_Type_Descriptor calls for a public key, but not an X509
+sign (Crypto_ID {crypto_ID_public_crypto_key = Just key})
+     _
+     (Signed_Hash_Type_Descriptor
+       {signed_Hash_Type_Descriptor_hash_type_descriptor = Just
+          Hash_Type_Descriptor
+          {hash_Type_Descriptor_sha2 = Just hash_sha2_descriptor}
+       ,signed_Hash_Type_Descriptor_crypto_id = Just
+          Crypto_ID_Type_Descriptor
+          {crypto_ID_Type_Descriptor_public_crypto_key = Just
+             descriptor@Public_Crypto_Key_Type_Descriptor
+               {public_Crypto_Key_Type_Descriptor_public_crypto_key_x509 = _}}})
+     _ _
+  = Left $ Hetcons_Exception_Descriptor_Does_Not_Match_Public_Crypto_Key
+             default_Descriptor_Does_Not_Match_Public_Crypto_Key {
+                descriptor_Does_Not_Match_Public_Crypto_Key_public_crypto_key_type_descriptor =
+                  descriptor
+               ,descriptor_Does_Not_Match_Public_Crypto_Key_public_crypto_key =
+                  key
+               ,descriptor_Does_Not_Match_Public_Crypto_Key_explanation =
+                  Just "You provided an X509, but asked for something else"}
+
+-- | If the Signed_Hash_Type_Descriptor calls for a crypto_ID which is not a public key
+sign crypto_id@(Crypto_ID {crypto_ID_public_crypto_key = Just _})
+     _
+     (Signed_Hash_Type_Descriptor
+       {signed_Hash_Type_Descriptor_hash_type_descriptor = Just
+          Hash_Type_Descriptor
+          {hash_Type_Descriptor_sha2 = Just hash_sha2_descriptor}
+       ,signed_Hash_Type_Descriptor_crypto_id = Just
+          descriptor@Crypto_ID_Type_Descriptor
+          {crypto_ID_Type_Descriptor_public_crypto_key = Nothing}})
+     _ _
+  = Left $ Hetcons_Exception_Descriptor_Does_Not_Match_Crypto_ID
+             default_Descriptor_Does_Not_Match_Crypto_ID {
+                descriptor_Does_Not_Match_Crypto_ID_crypto_id_type_descriptor =
+                  descriptor
+               ,descriptor_Does_Not_Match_Crypto_ID_crypto_id =
+                  crypto_id
+               ,descriptor_Does_Not_Match_Crypto_ID_explanation =
+                  Just "You provided a public key, but asked for something else"}
+
+
+-- | If the Signed_Hash_Type_Descriptor does not call for a crypto_ID
+sign crypto_id@(Crypto_ID {crypto_ID_public_crypto_key = Just _})
+     _
+     (Signed_Hash_Type_Descriptor
+       {signed_Hash_Type_Descriptor_hash_type_descriptor = Just
+          Hash_Type_Descriptor
+          {hash_Type_Descriptor_sha2 = Just hash_sha2_descriptor}
+       ,signed_Hash_Type_Descriptor_crypto_id = Nothing})
+     _ _
+  = Left $ Hetcons_Exception_No_Supported_Crypto_ID_Type_Descriptor_Provided
+             default_No_Supported_Crypto_ID_Type_Descriptor_Provided  {
+                no_Supported_Crypto_ID_Type_Descriptor_Provided_offending_crypto_id_type_descriptor = Nothing
+               ,no_Supported_Crypto_ID_Type_Descriptor_Provided_supported_crypto_id_type_descriptor = Just sUPPORTED_CRYPTO_ID_TYPE_DESCRIPTOR
+               ,no_Supported_Crypto_ID_Type_Descriptor_Provided_explanation = Just "You did not provide any Crypto_ID_Type_Descriptor at all"}
+
+-- | If the Signed_Hash_Type_Descriptor specifies a Hash, but it's not SHA2
+sign _ _
+     (Signed_Hash_Type_Descriptor
+       {signed_Hash_Type_Descriptor_hash_type_descriptor = Just
+          hash_type_descriptor@Hash_Type_Descriptor
+          {hash_Type_Descriptor_sha2 = Nothing}})
+     _ _
+  = Left $ Hetcons_Exception_No_Supported_Hash_Type_Descriptor_Provided
+             default_No_Supported_Hash_Type_Descriptor_Provided {
+                no_Supported_Hash_Type_Descriptor_Provided_offending_hash_type_descriptor =
+                  Just hash_type_descriptor
+               ,no_Supported_Hash_Type_Descriptor_Provided_supported_hash_type_descriptor =
+                  Just sUPPORTED_HASH_TYPE_DESCRIPTOR
+               ,no_Supported_Hash_Type_Descriptor_Provided_explanation =
+                  Just "Alas, I only support SHA2 hashes at this time."}
+
+-- | If the Signed_Hash_Type_Descriptor specifies a Hash, but it's not SHA2
+sign _ _ (Signed_Hash_Type_Descriptor {signed_Hash_Type_Descriptor_hash_type_descriptor = Nothing}) _ _ =
+  Left $ Hetcons_Exception_No_Supported_Hash_Type_Descriptor_Provided
+           default_No_Supported_Hash_Type_Descriptor_Provided  {
+             no_Supported_Hash_Type_Descriptor_Provided_offending_hash_type_descriptor = Nothing
+            ,no_Supported_Hash_Type_Descriptor_Provided_supported_hash_type_descriptor = Just sUPPORTED_HASH_TYPE_DESCRIPTOR
+            ,no_Supported_Hash_Type_Descriptor_Provided_explanation = Just "No Hash Type Descriptor provided"}
+
+{--
+
+public_crypto_key_match_type_descriptor :: Public_Crypto_Key -> Public_Crypto_Key_Type_Descriptor -> Maybe Hetcons_Exception
+-- | if there's an X509 certificate, and the descriptor allows one
+public_crypto_key_match_type_descriptor
+    (Public_Crypto_Key {public_Crypto_Key_public_crypto_key_x509 = Just _}})
+    (Public_Crypto_Key_Type_Descriptor{public_Crypto_Key_Type_Descriptor_public_crypto_key_x509 = Just True})
+  = Nothing
+-- | if there's a PGP certificate, and the descriptor allows one
+public_crypto_key_match_type_descriptor
+    (Public_Crypto_Key {public_Crypto_Key_public_crypto_key_pgp = Just _}})
+    (Public_Crypto_Key_Type_Descriptor{public_Crypto_Key_Type_Descriptor_public_crypto_key_pgp = Just True})
+  = Nothing
+-- | Otherwise, it's not a match
+public_crypto_key_match_type_descriptor key descriptor =
+  Hetcons_Exception_Descriptor_Does_Not_Match_Public_Crypto_Key
+    Descriptor_Does_Not_Match_Public_Crypto_Key {
+       descriptor_Does_Not_Match_Public_Crypto_Key_public_crypto_key_type_descriptor = descriptor
+      ,descriptor_Does_Not_Match_Public_Crypto_Key_public_crypto_key = key
+      ,descriptor_Does_Not_Match_Public_Crypto_Key_explanation = Just "This isn't an allowed key"
+
+
+
+data Crypto_ID_Type_Descriptor = Crypto_ID_Type_Descriptor  { crypto_ID_Type_Descriptor_public_crypto_key :: P.Maybe Public_Crypto_Key_Type_Descriptor
+  , crypto_ID_Type_Descriptor_crypto_id_hash :: P.Maybe Crypto_ID_Hash_Type_Descriptor
+data Crypto_ID_Hash = Crypto_ID_Hash  { crypto_ID_Hash_hash :: Hash
+  , crypto_ID_Hash_public_crypto_key_type_descriptor :: P.Maybe Public_Crypto_Key_Type_Descriptor
+crypto_id_hash_match_type_descriptor :: Crypto_ID_Hash -> Crypto_ID_Hash_Type_Descriptor -> Maybe Hetcons_Exception
+crypto_id_hash_match_type_descriptor
+  (Crypto_ID_Hash  {crypto_ID_Hash_hash = hash
+                   ,crypto_ID_Hash_public_crypto_key_type_descriptor = m_hash_key_type_descriptor1})
+  (Crypto_ID_Type_Descriptor {crypto_ID_Type_Descriptor_public_crypto_key = m_key_type_descriptor
+                             ,crypto_ID_Type_Descriptor_crypto_id_hash =
+                                 Crypto_ID_Hash_Type_Descriptor
+                                   {crypto_ID_Hash_Type_Descriptor_hash = hash_type_descriptor
+                                   ,crypto_ID_Hash_Type_Descriptor_public_crypto_key_type_descriptor = m_hash_key_type_descriptor2}})
+  = do{liftM (crypto_id_hash_match_type_descriptor crypto_id_hash) m_key_type_descriptor1
+      ;
+
+
+crypto_id_match_type_descriptor :: Crypto_ID -> Crypto_ID_Type_Descriptor -> Maybe Hetcons_Exception
+-- | If it's given a crypto key, and the descriptor allows that.
+crypto_id_match_type_descriptor (Crypto_ID {crypto_ID_public_crypto_key = Just key})
+                                (Crypto_ID_Type_Descriptor {crypto_ID_Type_Descriptor_public_crypto_key = Just descriptor})
+  = public_crypto_key_match_type_descriptor key descriptor
+-- | If it's given a hash, and the descriptor allows that.
+crypto_id_match_type_descriptor (Crypto_ID {crypto_ID_crypto_id_hash = Just hash})
+--}
