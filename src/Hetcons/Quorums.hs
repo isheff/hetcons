045e8a125ac797f21cb9e74c0263820bbef07a56
Isaac Sheff <isheff@cs.cornell.edu>
2017-07-20 18:56:32 -0400

    I think I have safely improved the quorum function by tracking only the edge case quorums

--- src/Hetcons/Quorums.hs
+++ src/Hetcons/Quorums.hs
@@ -97,34 +97,45 @@ verify_quorums' x@(Proposal_1a { proposal_1a_observers = Just (Observers {observ
                 ,invalid_Proposal_1a_explanation = Just "At this time, we require all proposals to carry Observers objects featuring quorums."}
 verify_quorums' (Proposal_1a { proposal_1a_observers = Just x }) = return x
 
+
+type Observer_Graph_Edge = HashSet (HashSet Participant_ID, HashSet Participant_ID)
+reduce :: Observer_Graph_Edge -> Observer_Graph_Edge
+reduce e = HashSet.filter (\(safe,live) -> (all (\(s,l) -> (((s,l) == (safe,live)) || (s /= intersection safe s) || (l /= intersection live l))) e)) e
+
+e_union :: Observer_Graph_Edge -> Observer_Graph_Edge -> Observer_Graph_Edge
+e_union x y = reduce $ union x y
+
+e_intersection :: Observer_Graph_Edge -> Observer_Graph_Edge -> Observer_Graph_Edge
+e_intersection x y = reduce $ fromList [(union sx sy, union lx ly) | (sx, lx) <- toList x,  (sy, ly) <- toList y]
+
 -- TODO: this is very slow, mostly due to the fact that we manually calculate powersets. We can probably do this much more efficiently.
 graph_to_quorums :: (MonadError Hetcons_Exception m) => Proposal_1a -> m Observers
 graph_to_quorums x@(Proposal_1a { proposal_1a_observers = Just x_observers@(Observers {observers_observer_graph = Just constraints})}) =
   let observers = toList $ union (HashSet.map observer_Trust_Constraint_observer_1 constraints) (HashSet.map observer_Trust_Constraint_observer_2 constraints)
       participants = unions $ toList $ union (HashSet.map observer_Trust_Constraint_safe constraints) (HashSet.map observer_Trust_Constraint_live constraints)
-      power_set = fromList $ map fromList $ filterM (const [True, False]) (toList participants)
-      power_constraints = fromList [(s,l) | s <- toList power_set, l <- toList power_set]
+      -- power_set = fromList $ map fromList $ filterM (const [True, False]) (toList participants)
+      -- power_constraints = fromList [(s,l) | s <- toList power_set, l <- toList power_set]
       constraints_for x y = HashSet.filter (\c -> (x == observer_Trust_Constraint_observer_1 c && y == observer_Trust_Constraint_observer_2 c) ||
                                                   (x == observer_Trust_Constraint_observer_2 c && y == observer_Trust_Constraint_observer_1 c) ) constraints
-
-      expanded_for x y = HashSet.filter (\(safe, live) -> (
-          any (\(Observer_Trust_Constraint{observer_Trust_Constraint_safe = c_safe, observer_Trust_Constraint_live = c_live}) ->
-                (c_safe == intersection c_safe safe) &&
-                (c_live == intersection c_live live))
+      expanded_for x y = reduce (HashSet.map
+              (\(Observer_Trust_Constraint{observer_Trust_Constraint_safe = c_safe, observer_Trust_Constraint_live = c_live}) ->
+                (c_safe,
+                 c_live))
                 -- TODO: I'm still concerned over whether we need a constraint about safe but non-live nodes.
                 -- You see, technically, a    safe live   node is normal,
                 --                     , an unsafe live   node is byzantine,
                 --                     , a    safe unlive node is crashed,
                 --                     , an unsafe unlive node is what? Really just a shorthand for this can crash or byzantine I guess, by the upward closure rule.
               (constraints_for x y)
-        )) power_constraints
+          )
       matrix = [[expanded_for x y | y <- observers] | x <- observers]
-      cog = floyd_warshall intersection union matrix
-      quorums = HashMap.fromList [(pid, ((unions $ map  (HashSet.map snd)  $ toList row):: (HashSet (HashSet Participant_ID)))) | (pid,row) <- (zip observers (toList matrix))]
+      cog = floyd_warshall e_intersection e_union matrix
+      quorums = HashMap.fromList [(pid, ((unions $ map  (HashSet.map snd)  $ toList row):: (HashSet (HashSet Participant_ID))))
+                                 | (pid,row) <- (zip observers (toList matrix))]
       valid_quorums = all (\(x,row) -> (all (\(y, sls) -> (all (\(safe, live) -> (
                       all (\qx -> (all (\qy -> (
-                        (qx /= intersection qx live) || -- this is where the crucial requirement is
-                        (qy /= intersection qy live) || --
+                        -- (qx /= intersection qx live) || -- this is where the crucial requirement is
+                        -- (qy /= intersection qy live) || --
                         (empty /= intersection safe (intersection qx qy))
                       )) $ quorums HashMap.! y)) $ quorums HashMap.! x))
                       sls)) $ zip observers $ toList row)) $ zip observers $ toList matrix
