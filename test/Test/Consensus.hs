3c888811c519a2cfa9b6a9174d8489cec8c79914
Isaac Sheff <isheff@cs.cornell.edu>
2017-07-28 16:43:58 -0400

    separated out test_4_participants into a seperate variable so the profiler can identify how much time it took

--- test/Test/Consensus.hs
+++ test/Test/Consensus.hs
@@ -312,6 +312,115 @@ launch_observer port = do
   ; return (new_port, now, address_book, cert, private)
   }
 
+-- | Separating this out into a separate variable helps the profiler identify how much time it took.
+test_4_participants =
+   TestLabel "Four participant Consensus" (
+     TestCase ( do
+       { private <- ByteString.readFile "test/key.pem"
+       ; private1 <- ByteString.readFile "test/key1.pem"
+       ; private2 <- ByteString.readFile "test/key2.pem"
+       ; private3 <- ByteString.readFile "test/key3.pem"
+       ; private4 <- ByteString.readFile "test/key4.pem"
+       ; private6 <- ByteString.readFile "test/key6.pem"
+       ; cert <- ByteString.readFile "test/cert.pem"
+       ; certs' <- mapM (\i -> ByteString.readFile $ "test/cert" ++ (show i) ++ ".pem") [1..6]
+       ; let (ids :: [Participant_ID]) = map (uncurry sample_id) (zip (cert:certs') [85020..85026])
+       ; proof_receipt <- newEmptyMVar
+       ; participant1 <- new_participant (participant_ID_crypto_id (ids!!1)) private1
+       ; participant_thread1<- participant_server participant1 85021
+       ; participant2 <- new_participant (participant_ID_crypto_id (ids!!2)) private2
+       ; participant_thread2 <- participant_server participant2 85022
+       ; participant3 <- start_State >>= (\(sv :: Participant_State_Var) -> return (participant1{hetcons_Server_state_var = sv
+                                                              ,hetcons_Server_crypto_id = (participant_ID_crypto_id (ids!!3))
+                                                              ,hetcons_Server_private_key = private3}))
+       ; participant_thread3<- participant_server participant3 85023
+       ; participant4 <- start_State >>= (\(sv :: Participant_State_Var) -> return (participant1{hetcons_Server_state_var = sv
+                                                              ,hetcons_Server_crypto_id = (participant_ID_crypto_id (ids!!4))
+                                                              ,hetcons_Server_private_key = private4}))
+       ; participant_thread4<- participant_server participant4 85024
+       ; observer_1_state <- start_State
+       ; let observer1_datum = Observer {
+           observer_hetcons_server = (Hetcons_Server {
+                                       hetcons_Server_crypto_id = (participant_ID_crypto_id (ids!!0))
+                                      ,hetcons_Server_private_key = private
+                                      ,hetcons_Server_address_book = hetcons_Server_address_book participant1
+                                      ,hetcons_Server_state_var = observer_1_state
+                                      ,hetcons_Server_verify_1a = hetcons_Server_verify_1a participant1
+                                      ,hetcons_Server_verify_1b = hetcons_Server_verify_1b participant1
+                                      ,hetcons_Server_verify_2a = hetcons_Server_verify_2a participant1
+                                      ,hetcons_Server_verify_2b = hetcons_Server_verify_2b participant1
+                                      ,hetcons_Server_verify_proof = hetcons_Server_verify_proof participant1
+                                      ,hetcons_Server_verify_quorums = hetcons_Server_verify_quorums participant1
+                                      })
+           ,do_on_consensus = putMVar proof_receipt}
+       ; observer1 <- observer_server observer1_datum 85020
+       ; observer_2_state <- start_State
+       ; let observer2_datum = Observer {
+           observer_hetcons_server = (Hetcons_Server {
+                                       hetcons_Server_crypto_id = (participant_ID_crypto_id (ids!!6))
+                                      ,hetcons_Server_private_key = private6
+                                      ,hetcons_Server_address_book = hetcons_Server_address_book participant1
+                                      ,hetcons_Server_state_var = observer_2_state
+                                      ,hetcons_Server_verify_1a = hetcons_Server_verify_1a participant1
+                                      ,hetcons_Server_verify_1b = hetcons_Server_verify_1b participant1
+                                      ,hetcons_Server_verify_2a = hetcons_Server_verify_2a participant1
+                                      ,hetcons_Server_verify_2b = hetcons_Server_verify_2b participant1
+                                      ,hetcons_Server_verify_proof = hetcons_Server_verify_proof participant1
+                                      ,hetcons_Server_verify_quorums = hetcons_Server_verify_quorums participant1
+                                      })
+           ,do_on_consensus = putMVar proof_receipt}
+       ; observer2<- observer_server observer2_datum 85026
+       ; address_book <- default_Address_Book
+       ; now <- current_nanoseconds
+       ; let message_1a = default_Proposal_1a {
+                            proposal_1a_value = default_Value {
+                                                   value_value_payload = ByteString.singleton 42
+                                                  ,value_slot = 6}
+                           ,proposal_1a_timestamp = now
+                           ,proposal_1a_observers = Just default_Observers {
+                                                           observers_observer_graph = Just $ fromList $ [
+                                                                   default_Observer_Trust_Constraint  {
+                                                                     observer_Trust_Constraint_observer_1 = ids!!0
+                                                                    ,observer_Trust_Constraint_observer_2 = ids!!6
+                                                                    ,observer_Trust_Constraint_safe = fromList $ [ids!!1, ids!!2, ids!!3        ]
+                                                                    ,observer_Trust_Constraint_live = fromList $ [ids!!1, ids!!2, ids!!3        ]}
+                                                                  ,default_Observer_Trust_Constraint  {
+                                                                     observer_Trust_Constraint_observer_1 = ids!!0
+                                                                    ,observer_Trust_Constraint_observer_2 = ids!!6
+                                                                    ,observer_Trust_Constraint_safe = fromList $ [ids!!1, ids!!2,         ids!!4]
+                                                                    ,observer_Trust_Constraint_live = fromList $ [ids!!1, ids!!2,         ids!!4]}
+                                                                  ,default_Observer_Trust_Constraint  {
+                                                                     observer_Trust_Constraint_observer_1 = ids!!0
+                                                                    ,observer_Trust_Constraint_observer_2 = ids!!6
+                                                                    ,observer_Trust_Constraint_safe = fromList $ [ids!!1,         ids!!3, ids!!4]
+                                                                    ,observer_Trust_Constraint_live = fromList $ [ids!!1,         ids!!3, ids!!4]}
+                                                                  ,default_Observer_Trust_Constraint  {
+                                                                     observer_Trust_Constraint_observer_1 = ids!!0
+                                                                    ,observer_Trust_Constraint_observer_2 = ids!!6
+                                                                    ,observer_Trust_Constraint_safe = fromList $ [        ids!!2, ids!!3, ids!!4]
+                                                                    ,observer_Trust_Constraint_live = fromList $ [        ids!!2, ids!!3, ids!!4]}]}}
+       ; (Right signed_1a) <- sample_sign $ message_1a
+       ; let (Right (v1a :: (Verified Recursive_1a))) = verify signed_1a
+       ; send_thread <- forkIO (catch (catch (send_Message_IO address_book v1a)
+                                      (\(exception :: Hetcons_Exception) -> assertBool ("Hetcons Exception Caught: " ++ (show exception)) False))
+                               (\(exception :: SomeException) -> (assertBool ("Exception Caught: " ++ (show exception)) ((show exception) == "thread killed"))))
+       ; received_proof <- takeMVar proof_receipt
+       ; assertEqual "incorrect observers proven" (fromList ["localhost:85020","localhost:85026"]) $
+           foldr (\n x -> insert ((domain_name n) ++ ":"++ (show $ address_port_number $ participant_ID_address n)) x) empty $ observers_proven received_proof
+       ; assertEqual "wrong value proven" (extract_value message_1a) (extract_value received_proof)
+       ; received_proof2 <- takeMVar proof_receipt
+       ; assertEqual "incorrect observers proven" (fromList ["localhost:85020","localhost:85026"]) $
+           foldr (\n x -> insert ((domain_name n) ++ ":"++ (show $ address_port_number $ participant_ID_address n)) x) empty $ observers_proven received_proof2
+       ; assertEqual "wrong value proven" (extract_value message_1a) (extract_value received_proof2)
+       -- print out clock time taken with ; current_nanoseconds >>= (putStrLn . ("\n"++) . show . (\x -> (fromIntegral (x - now))/(1000000000.0)))
+       -- The messages sent are:
+       -- 1A:                  1 sender,  1 signer,  4 recipients, 1 instance  per signer =  4 messages
+       -- 1B:                  4 senders, 4 signers, 4 recipients, 1 instance  per signer = 64 messages
+       -- 2B:                  3 senders, 4 signers, 2 recipients, 2 instances per signer = 48 messages
+       -- PoC featuring 3 2Bs: 2 senders, 1 signer,  2 recipients, 1 instance  per signer = 12 messages
+       -- PoC featuring 4 2Bs: 2 senders, 1 signer,  2 recipients, 1 instance  per signer = 16 messages
+       --                                                                           TOTAL = 144 messages
+       }))
 
 consensus_tests = TestList [
 
@@ -427,106 +536,7 @@ consensus_tests = TestList [
        ; assertEqual "wrong value proven" (extract_value message_1a) (extract_value received_proof2)
        }))
 
-  ,TestLabel "Four participant Consensus" (
-     TestCase ( do
-       { private <- ByteString.readFile "test/key.pem"
-       ; private1 <- ByteString.readFile "test/key1.pem"
-       ; private2 <- ByteString.readFile "test/key2.pem"
-       ; private3 <- ByteString.readFile "test/key3.pem"
-       ; private4 <- ByteString.readFile "test/key4.pem"
-       ; private6 <- ByteString.readFile "test/key6.pem"
-       ; cert <- ByteString.readFile "test/cert.pem"
-       ; certs' <- mapM (\i -> ByteString.readFile $ "test/cert" ++ (show i) ++ ".pem") [1..6]
-       ; let (ids :: [Participant_ID]) = map (uncurry sample_id) (zip (cert:certs') [85020..85026])
-       ; proof_receipt <- newEmptyMVar
-       ; participant1 <- new_participant (participant_ID_crypto_id (ids!!1)) private1
-       ; participant_thread1<- participant_server participant1 85021
-       ; participant2 <- new_participant (participant_ID_crypto_id (ids!!2)) private2
-       ; participant_thread2 <- participant_server participant2 85022
-       ; participant3 <- start_State >>= (\(sv :: Participant_State_Var) -> return (participant1{hetcons_Server_state_var = sv
-                                                              ,hetcons_Server_crypto_id = (participant_ID_crypto_id (ids!!3))
-                                                              ,hetcons_Server_private_key = private3}))
-       ; participant_thread3<- participant_server participant3 85023
-       ; participant4 <- start_State >>= (\(sv :: Participant_State_Var) -> return (participant1{hetcons_Server_state_var = sv
-                                                              ,hetcons_Server_crypto_id = (participant_ID_crypto_id (ids!!4))
-                                                              ,hetcons_Server_private_key = private4}))
-       ; participant_thread4<- participant_server participant4 85024
-       ; observer_1_state <- start_State
-       ; let observer1_datum = Observer {
-           observer_hetcons_server = (Hetcons_Server {
-                                       hetcons_Server_crypto_id = (participant_ID_crypto_id (ids!!0))
-                                      ,hetcons_Server_private_key = private
-                                      ,hetcons_Server_address_book = hetcons_Server_address_book participant1
-                                      ,hetcons_Server_state_var = observer_1_state
-                                      ,hetcons_Server_verify_1a = hetcons_Server_verify_1a participant1
-                                      ,hetcons_Server_verify_1b = hetcons_Server_verify_1b participant1
-                                      ,hetcons_Server_verify_2a = hetcons_Server_verify_2a participant1
-                                      ,hetcons_Server_verify_2b = hetcons_Server_verify_2b participant1
-                                      ,hetcons_Server_verify_proof = hetcons_Server_verify_proof participant1
-                                      ,hetcons_Server_verify_quorums = hetcons_Server_verify_quorums participant1
-                                      })
-           ,do_on_consensus = putMVar proof_receipt}
-       ; observer1 <- observer_server observer1_datum 85020
-       ; observer_2_state <- start_State
-       ; let observer2_datum = Observer {
-           observer_hetcons_server = (Hetcons_Server {
-                                       hetcons_Server_crypto_id = (participant_ID_crypto_id (ids!!6))
-                                      ,hetcons_Server_private_key = private6
-                                      ,hetcons_Server_address_book = hetcons_Server_address_book participant1
-                                      ,hetcons_Server_state_var = observer_2_state
-                                      ,hetcons_Server_verify_1a = hetcons_Server_verify_1a participant1
-                                      ,hetcons_Server_verify_1b = hetcons_Server_verify_1b participant1
-                                      ,hetcons_Server_verify_2a = hetcons_Server_verify_2a participant1
-                                      ,hetcons_Server_verify_2b = hetcons_Server_verify_2b participant1
-                                      ,hetcons_Server_verify_proof = hetcons_Server_verify_proof participant1
-                                      ,hetcons_Server_verify_quorums = hetcons_Server_verify_quorums participant1
-                                      })
-           ,do_on_consensus = putMVar proof_receipt}
-       ; observer2<- observer_server observer2_datum 85026
-       ; address_book <- default_Address_Book
-       ; now <- current_nanoseconds
-       ; let message_1a = default_Proposal_1a {
-                            proposal_1a_value = default_Value {
-                                                   value_value_payload = ByteString.singleton 42
-                                                  ,value_slot = 6}
-                           ,proposal_1a_timestamp = now
-                           ,proposal_1a_observers = Just default_Observers {
-                                                           observers_observer_graph = Just $ fromList $ [
-                                                                   default_Observer_Trust_Constraint  {
-                                                                     observer_Trust_Constraint_observer_1 = ids!!0
-                                                                    ,observer_Trust_Constraint_observer_2 = ids!!6
-                                                                    ,observer_Trust_Constraint_safe = fromList $ [ids!!1, ids!!2, ids!!3        ]
-                                                                    ,observer_Trust_Constraint_live = fromList $ [ids!!1, ids!!2, ids!!3        ]}
-                                                                  ,default_Observer_Trust_Constraint  {
-                                                                     observer_Trust_Constraint_observer_1 = ids!!0
-                                                                    ,observer_Trust_Constraint_observer_2 = ids!!6
-                                                                    ,observer_Trust_Constraint_safe = fromList $ [ids!!1, ids!!2,         ids!!4]
-                                                                    ,observer_Trust_Constraint_live = fromList $ [ids!!1, ids!!2,         ids!!4]}
-                                                                  ,default_Observer_Trust_Constraint  {
-                                                                     observer_Trust_Constraint_observer_1 = ids!!0
-                                                                    ,observer_Trust_Constraint_observer_2 = ids!!6
-                                                                    ,observer_Trust_Constraint_safe = fromList $ [ids!!1,         ids!!3, ids!!4]
-                                                                    ,observer_Trust_Constraint_live = fromList $ [ids!!1,         ids!!3, ids!!4]}
-                                                                  ,default_Observer_Trust_Constraint  {
-                                                                     observer_Trust_Constraint_observer_1 = ids!!0
-                                                                    ,observer_Trust_Constraint_observer_2 = ids!!6
-                                                                    ,observer_Trust_Constraint_safe = fromList $ [        ids!!2, ids!!3, ids!!4]
-                                                                    ,observer_Trust_Constraint_live = fromList $ [        ids!!2, ids!!3, ids!!4]}]}}
-       ; (Right signed_1a) <- sample_sign $ message_1a
-       ; let (Right (v1a :: (Verified Recursive_1a))) = verify signed_1a
-       ; send_thread <- forkIO (catch (catch (send_Message_IO address_book v1a)
-                                      (\(exception :: Hetcons_Exception) -> assertBool ("Hetcons Exception Caught: " ++ (show exception)) False))
-                               (\(exception :: SomeException) -> (assertBool ("Exception Caught: " ++ (show exception)) ((show exception) == "thread killed"))))
-       ; received_proof <- takeMVar proof_receipt
-       ; assertEqual "incorrect observers proven" (fromList ["localhost:85020","localhost:85026"]) $
-           foldr (\n x -> insert ((domain_name n) ++ ":"++ (show $ address_port_number $ participant_ID_address n)) x) empty $ observers_proven received_proof
-       ; assertEqual "wrong value proven" (extract_value message_1a) (extract_value received_proof)
-       ; received_proof2 <- takeMVar proof_receipt
-       ; assertEqual "incorrect observers proven" (fromList ["localhost:85020","localhost:85026"]) $
-           foldr (\n x -> insert ((domain_name n) ++ ":"++ (show $ address_port_number $ participant_ID_address n)) x) empty $ observers_proven received_proof2
-       ; assertEqual "wrong value proven" (extract_value message_1a) (extract_value received_proof2)
-       -- print out clock time taken with ; current_nanoseconds >>= (putStrLn . ("\n"++) . show . (\x -> (fromIntegral (x - now))/(1000000000.0)))
-       }))
+  ,test_4_participants
 
   ,TestLabel "Three participant Consensus with a crash failure" (
      TestCase ( do
