f74e75a455b6f198bf3bf11249d303533ea40990
Isaac Sheff <isheff@cs.cornell.edu>
2017-07-31 15:37:38 -0400

    cleaned up imports

--- src/Hetcons/Participant.hs
+++ src/Hetcons/Participant.hs
@@ -4,85 +4,50 @@
 
 module Hetcons.Participant (Participant, new_participant, participant_server, basic_participant_server, current_nanoseconds ) where
 
-import Hetcons.Conflicting_2as    (conflicting_2as)
-import Hetcons.Contains_Value     (Contains_1a, Contains_1bs, extract_1bs, extract_1a, extract_value, extract_ballot, extract_observer_quorums)
-import Hetcons.Hetcons_Exception  (Hetcons_Exception)
-import Hetcons.Hetcons_State      (Hetcons_State, Participant_State, Observer_State, Participant_State_Var, modify_and_read, default_State, start_State)
-import Hetcons.Instances_1a ()
-import Hetcons.Instances_1b_2a ()
-import Hetcons.Instances_2b ()
-import Hetcons.Instances_Proof_of_Consensus ()
-import Hetcons.Receive()
+import Hetcons.Contains_Value ( Contains_1a, extract_1a )
+import Hetcons.Hetcons_State ( Participant_State, start_State )
+import Hetcons.Receive ()
 import Hetcons.Receive_Message
-  (Hetcons_Transaction
-    ,run_Hetcons_Transaction_IO
-    ,get_state
-    ,put_state
-    ,update_state
-    ,get_my_crypto_id
-    ,get_my_private_key
-  ,Add_Sent
-    ,add_sent
-  ,Receivable
-    ,receive
-  ,Sendable
-    ,send
-  ,Hetcons_Server(Hetcons_Server)
-    ,hetcons_Server_crypto_id
-    ,hetcons_Server_private_key
-    ,hetcons_Server_address_book
-    ,hetcons_Server_state_var
-    ,hetcons_Server_verify_1a
-    ,hetcons_Server_verify_1b
-    ,hetcons_Server_verify_2a
-    ,hetcons_Server_verify_2b
-    ,hetcons_Server_verify_proof
-    ,hetcons_Server_verify_quorums
-  )
-import Hetcons.Send               ()
-import Hetcons.Send_Message_IO    (Address_Book, default_Address_Book, send_Message_IO)
-import Hetcons.Signed_Message     (Verified, original, signed, sign, verify, non_recursive, Recursive_1a, Recursive_1b, Recursive_2a, Recursive_2b, Recursive_Proof_of_Consensus, Parsable)
-
-import Hetcons_Consts             (sUPPORTED_SIGNED_HASH_TYPE_DESCRIPTOR)
-import Hetcons_Participant (process)
-import Hetcons_Participant_Iface (Hetcons_Participant_Iface
-                                   ,ping
-                                   ,proposal_1a
-                                   ,phase_1b)
-import Hetcons_Types              (Timestamp
-                                  ,Crypto_ID
-                                  ,default_Phase_1b
-                                  ,phase_1b_proposal
-                                  ,phase_1b_conflicting_phase2as
-                                  ,Signed_Message
-                                  ,default_Phase_2a
-                                  ,Phase_2a(Phase_2a)
-                                    ,phase_2a_phase_1bs
-                                  ,default_Phase_2b
-                                  ,phase_2b_phase_1bs
-                                  ,default_Proof_of_Consensus
-                                  ,proof_of_Consensus_phase_2bs
-                                  ,signed_Hash_crypto_id
-                                  ,signed_Message_signature
-                                  ,proposal_1a_timestamp
-                                  )
-
-import Control.Concurrent (forkIO, ThreadId, threadDelay)
-import qualified Control.Concurrent.Map as CMap (Map, empty, lookup)
-import Control.Exception.Base     (throw)
-import Control.Monad              (mapM, mapM_)
-import Control.Monad.Except       (throwError, catchError, MonadError)
-import Control.Monad.Reader       (MonadReader, Reader, runReader, reader, ask, local)
-import Control.Monad.Trans.Either (EitherT, runEitherT)
-import Control.Monad.State        (StateT, runStateT, get, put,state)
-import Crypto.Random              (drgNew)
-import Data.ByteString.Lazy       (ByteString)
-import Data.HashSet               (HashSet, insert, toList, fromList,  empty, member)
-import qualified Data.HashSet as HashSet (map, filter)
-import Data.Serialize             (Serialize)
-import Data.Thyme.Clock           (getCurrentTime, UTCTime, UTCView(UTCTime))
-import Data.Thyme.Time.Core       (toThyme, fromThyme, diffUTCTime, toMicroseconds, fromMicroseconds, fromGregorian)
-import Thrift.Server (runBasicServer)
+    ( Hetcons_Server(Hetcons_Server)
+        ,hetcons_Server_crypto_id
+        ,hetcons_Server_private_key
+        ,hetcons_Server_address_book
+        ,hetcons_Server_state_var
+        ,hetcons_Server_verify_1a
+        ,hetcons_Server_verify_1b
+        ,hetcons_Server_verify_2a
+        ,hetcons_Server_verify_2b
+        ,hetcons_Server_verify_proof
+        ,hetcons_Server_verify_quorums
+     ,Receivable(receive)
+     ,run_Hetcons_Transaction_IO )
+import Hetcons.Send_Message_IO ( default_Address_Book )
+import Hetcons.Signed_Message
+    ( Recursive_1b
+     ,Verified
+     ,Recursive_1a
+     ,Recursive(non_recursive)
+     ,Recursive_Proof_of_Consensus
+     ,Monad_Verify(verify)
+     ,original )
+import Hetcons_Participant ( process )
+import Hetcons_Participant_Iface
+    ( Hetcons_Participant_Iface, ping, proposal_1a, phase_1b )
+import Hetcons_Types
+    ( Proposal_1a(proposal_1a_timestamp), Crypto_ID, Timestamp )
+import Control.Concurrent ( forkIO, ThreadId, threadDelay )
+import qualified Control.Concurrent.Map as CMap ( empty )
+import Data.ByteString.Lazy ( ByteString )
+import Data.Thyme.Clock
+    ( getCurrentTime, UTCTime, UTCView(UTCTime) )
+import Data.Thyme.Time.Core
+    ( toThyme
+     ,fromThyme
+     ,diffUTCTime
+     ,toMicroseconds
+     ,fromMicroseconds
+     ,fromGregorian )
+import Thrift.Server ( runBasicServer )
 
 type Participant = Hetcons_Server Participant_State
 
