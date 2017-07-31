b16306377ccffec0627d7bd50c032854491de3e5
Isaac Sheff <isheff@cs.cornell.edu>
2017-07-31 17:54:28 -0400

    cleaned up imports

--- src/Hetcons/Serializable.hs
+++ src/Hetcons/Serializable.hs
@@ -8,54 +8,33 @@
 
 module Hetcons.Serializable () where
 
+import Hetcons_Types
+    ( Signed_Message
+       ,encode_Signed_Message
+       ,decode_Signed_Message
+     ,Proposal_1a
+       ,encode_Proposal_1a
+       ,decode_Proposal_1a
+     ,Proof_of_Consensus
+       ,encode_Proof_of_Consensus
+       ,decode_Proof_of_Consensus
+     ,Phase_2b
+       ,encode_Phase_2b
+       ,decode_Phase_2b
+     ,Phase_2a
+       ,encode_Phase_2a
+       ,decode_Phase_2a
+     ,Phase_1b
+       ,encode_Phase_1b
+       ,decode_Phase_1b )
 
-import Hetcons_Types (Proposal_1a
-                        ,proposal_1a_observers
-                        ,encode_Proposal_1a
-                        ,decode_Proposal_1a
-                     ,Phase_1b
-                        ,phase_1b_proposal
-                        ,phase_1b_conflicting_phase2as
-                        ,encode_Phase_1b
-                        ,decode_Phase_1b
-                     ,Phase_2a
-                        ,phase_2a_phase_1bs
-                        ,default_Phase_2a
-                        ,encode_Phase_2a
-                        ,decode_Phase_2a
-                     ,Phase_2b
-                        ,phase_2b_phase_1bs
-                        ,default_Phase_2b
-                        ,encode_Phase_2b
-                        ,decode_Phase_2b
-                     ,Proof_of_Consensus
-                        ,proof_of_Consensus_phase_2bs
-                        ,default_Proof_of_Consensus
-                        ,encode_Proof_of_Consensus
-                        ,decode_Proof_of_Consensus
-                     ,Signed_Message
-                        ,encode_Signed_Message
-                        ,decode_Signed_Message
-                     )
-
-import           Control.Monad          (liftM, mapM_)
-import           Data.ByteString.Lazy   (unpack)
-import           Data.Foldable          (length)
-import           Data.Serialize         (Serialize
-                                        ,get
-                                        ,put)
-import           Data.Serialize.Get     (remaining, getLazyByteString)
-import           Data.Serialize.Put     (putWord8)
-import           Thrift.Protocol.Binary (BinaryProtocol(BinaryProtocol))
-import           Thrift.Transport.Empty (EmptyTransport(EmptyTransport))
---
--- | Serialize and deserialize this Thrift type using Thrift's functions.
--- | This should probably be done natively by Thrift.
-instance Serialize Signed_Message where
-  put = (mapM_ putWord8) . unpack . (encode_Signed_Message (BinaryProtocol EmptyTransport))
-  get = liftM (decode_Signed_Message (BinaryProtocol EmptyTransport)) (
-         do { length <- remaining
-            ; getLazyByteString (fromIntegral length)})
+import Control.Monad ( liftM, mapM_ )
+import Data.ByteString.Lazy ( unpack )
+import Data.Serialize ( Serialize, get, put )
+import Data.Serialize.Get ( remaining, getLazyByteString )
+import Data.Serialize.Put ( putWord8 )
+import Thrift.Protocol.Binary ( BinaryProtocol(BinaryProtocol) )
+import Thrift.Transport.Empty ( EmptyTransport(EmptyTransport) )
 
 -- | Serialize and deserialize this Thrift type using Thrift's functions.
 -- | This should probably be done natively by Thrift.
