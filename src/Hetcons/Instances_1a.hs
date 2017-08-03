a5c2a2f32ab768c511e575cf50acedf9f3fc6705
Isaac Sheff <isheff@cs.cornell.edu>
2017-08-03 18:19:28 -0400

    added documentation for Instances_1a. At some point I should got through and minimize the compiler extensions for each sub-module

--- src/Hetcons/Instances_1a.hs
+++ src/Hetcons/Instances_1a.hs
@@ -1,11 +1,8 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE UndecidableInstances #-}
 
+-- | The properties of a 1A message,such as how to parse it, extract valuese from it, etc.
 module Hetcons.Instances_1a () where
 
 import Hetcons.Contains_Value
@@ -32,17 +29,19 @@ import Data.Hashable ( Hashable, hashWithSalt )
 import Thrift.Protocol.Binary ( BinaryProtocol(BinaryProtocol) )
 import Thrift.Transport.Empty ( EmptyTransport(EmptyTransport) )
 
+-- | Hash a Recursive_1a by hashing its non-recursive version
 instance Hashable Recursive_1a where
   hashWithSalt s x = hashWithSalt s ((non_recursive x) :: Proposal_1a)
 
+-- | The non-recursive version of a Recursive_1a is a Proposal_1a
 instance Recursive Proposal_1a Recursive_1a where
   non_recursive = recursive_1a_non_recursive
 
-
+-- | Encode a Proposal_1a to a bytestring using Thrift
 instance {-# OVERLAPPING #-} Encodable Proposal_1a where
   encode = encode_Proposal_1a (BinaryProtocol EmptyTransport)
 
--- | For a 1a object, we verify observer graph, and fill in quorums
+-- | To parse a Recursive_1a object, we verify observer graph, and fill in quorums
 instance {-# OVERLAPPING #-} Parsable Recursive_1a where
   parse payload =
     do { non_recursive <- parse payload
@@ -51,9 +50,14 @@ instance {-# OVERLAPPING #-} Parsable Recursive_1a where
               recursive_1a_non_recursive = non_recursive
        ,recursive_1a_filled_in = non_recursive {proposal_1a_observers = Just filled_in}}}
 
+-- | A 1A contains a 1A, itself.
 instance {-# OVERLAPPING #-} Contains_1a (Verified Recursive_1a) where
   extract_1a = id
+
+-- | A Proposal_1a contains a value
 instance {-# OVERLAPPING #-} Contains_Value Proposal_1a where
   extract_value = proposal_1a_value
+
+-- | a Recursive_1a contains a value
 instance {-# OVERLAPPING #-} Contains_Value Recursive_1a where
   extract_value = extract_value . recursive_1a_filled_in
